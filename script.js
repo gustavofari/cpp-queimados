8e1661606bc5ddba72fec407bfe680ba7167dcd0
Gustavo Farias <gustavogarcia266@hotmail.com>
2023-07-24 09:32:27 -0300

    remove hamburguer

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const nav = document.querySelector(".navbar");
-const btnMenu = document.querySelector(".btn-menu");
-const menu = document.querySelector(".nav-menu");
+// const btnMenu = document.querySelector(".btn-menu");
+// const menu = document.querySelector(".nav-menu");
 
-function handleButtonClick(event) {
-  if (event.type === "touchstart") event.preventDefault();
-  event.stopPropagation();
-  nav.classList.toggle("active");
-  handleClickOutside(menu, () => {
-    nav.classList.remove("active");
-    setAria();
-  });
-  handleClickOutside(btnMenu, () => {
-    nav.classList.remove("active");
-    setAria();
-  });
-  setAria();
-}
+// function handleButtonClick(event) {
+//   if (event.type === "touchstart") event.preventDefault();
+//   event.stopPropagation();
+//   nav.classList.toggle("active");
+//   handleClickOutside(menu, () => {
+//     nav.classList.remove("active");
+//     setAria();
+//   });
+//   handleClickOutside(btnMenu, () => {
+//     nav.classList.remove("active");
+//     setAria();
+//   });
+//   setAria();
+// }
 
-function handleClickOutside(targetElement, callback) {
-  const html = document.documentElement;
-  function handleHTMLClick(event) {
-    if (!targetElement.contains(event.target)) {
-      targetElement.removeAttribute("data-target");
-      html.removeEventListener("click", handleHTMLClick);
-      html.removeEventListener("touchstart", handleHTMLClick);
-      callback();
-    }
-  }
-  if (!targetElement.hasAttribute("data-target")) {
-    html.addEventListener("click", handleHTMLClick);
-    html.addEventListener("touchstart", handleHTMLClick);
-    targetElement.setAttribute("data-target", "");
-  }
-}
+// function handleClickOutside(targetElement, callback) {
+//   const html = document.documentElement;
+//   function handleHTMLClick(event) {
+//     if (!targetElement.contains(event.target)) {
+//       targetElement.removeAttribute("data-target");
+//       html.removeEventListener("click", handleHTMLClick);
+//       html.removeEventListener("touchstart", handleHTMLClick);
+//       callback();
+//     }
+//   }
+//   if (!targetElement.hasAttribute("data-target")) {
+//     html.addEventListener("click", handleHTMLClick);
+//     html.addEventListener("touchstart", handleHTMLClick);
+//     targetElement.setAttribute("data-target", "");
+//   }
+// }
 
-function setAria() {
-  const isActive = nav.classList.contains("active");
-  btnMenu.setAttribute("aria-expanded", isActive);
-  if (isActive) {
-    btnMenu.setAttribute("aria-label", "Fechar Menu");
-  } else {
-    btnMenu.setAttribute("aria-label", "Abrir Menu");
-  }
-}
+// function setAria() {
+//   const isActive = nav.classList.contains("active");
+//   btnMenu.setAttribute("aria-expanded", isActive);
+//   if (isActive) {
+//     btnMenu.setAttribute("aria-label", "Fechar Menu");
+//   } else {
+//     btnMenu.setAttribute("aria-label", "Abrir Menu");
+//   }
+// }
 
-btnMenu.addEventListener("click", handleButtonClick);
-btnMenu.addEventListener("touchstart", handleButtonClick);
+// btnMenu.addEventListener("click", handleButtonClick);
+// btnMenu.addEventListener("touchstart", handleButtonClick);
 
 function typeScreen(text, i = 0) {
   const element = document.getElementById("text-2");
